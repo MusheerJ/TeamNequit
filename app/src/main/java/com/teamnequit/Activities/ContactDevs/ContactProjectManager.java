bdbe9f0d9bd948f6b011493cbd80dd79d8349e02
MusheerAhmed <musheerjamadar1024@gmail.com>
2021-08-12 08:58:36 +0530

    version 0.0

---
+++ app/src/main/java/com/teamnequit/Activities/ContactDevs/ContactProjectManager.java
@@ -0,0 +1,14 @@
+package com.teamnequit.Activities.ContactDevs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactProjectManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_project_manager);
+    }
+}
---
+++ app/src/main/java/com/teamnequit/Activities/KPIActivity.java
@@ -0,0 +1,14 @@
+package com.teamnequit.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KPIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_k_p_i);
+    }
+}
